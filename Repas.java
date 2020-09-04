369d643cf6b695e69f4735f184338bd2340f7c11
sorayafl <soraya.flissi.sio@gmail.com>
2020-09-04 16:52:03 +0200

    implémentation du constructeur Repas

---
+++ Repas.java
@@ -0,0 +1,27 @@
+package fr.awa.awajaba;
+
+import java.time.LocalDate;
+
+public class Repas {
+	
+	private int numero;
+	private LocalDate date;
+	public Repas(int numero, LocalDate date) {
+		super();
+		this.numero = numero;
+		this.date = date;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	} 
+
+}
