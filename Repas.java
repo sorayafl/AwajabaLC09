9c664af34269f7691dccccafe815d39b06999666
sorayafl <soraya.flissi.sio@gmail.com>
2020-09-07 10:17:29 +0200

    C2

--- Repas.java
+++ Repas.java
@@ -4,5 +4,13 @@ import java.time.LocalDate;
 
 public class Repas {
 
+	private int numero ;
+	private LocalDate date ;
+	
+	public Repas(int numero, LocalDate date) {
+		super();
+		this.numero = numero;
+		this.date = date;
+	}
 
 }
