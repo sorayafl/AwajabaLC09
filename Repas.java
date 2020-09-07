91eb01235e385836c71a6e49c33edd6d62a66d94
sorayafl <soraya.flissi.sio@gmail.com>
2020-09-07 10:17:55 +0200

    C3

--- Repas.java
+++ Repas.java
@@ -12,5 +12,21 @@ public class Repas {
 		this.numero = numero;
 		this.date = date;
 	}
+		
+	public LocalDate getDate() {
+		return date;
+	}
 
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
 }
